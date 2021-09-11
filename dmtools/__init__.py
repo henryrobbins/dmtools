260ef6ebdd66ec879951e589bda3b4f33e950e84
Henry Robbins <hwr26@cornell.edu>
2021-09-10 22:58:28 -0400

    Added description to __init__.py.

--- dmtools/__init__.py
+++ dmtools/__init__.py
@@ -1,7 +1,13 @@
 """
 dmtools
+=======
 
-TODO: Add description.
+dmtools (Digital Media Tools) is a Python package providing low-level tools for
+working with digital media programmatically. The netpbm module allows one to
+read and create Netpbm images. Color space transformations can be done with the
+colorspace module. Using ffmpeg, the animation module can export .mp4 videos
+formed from a list of images and the sound module can be used to add sound to
+these videos as well. Lastly, ASCII art can be produced with the ascii module.
 """
 
 __author__ = 'Henry Robbins'
